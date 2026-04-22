20ea8e4f6bd6ccef22fcd70a9821f5175e741301
Alexa Villavicencio <avillavicencio@centro.edu.mx>
2026-04-21 20:18:48 -0600

    Refactor cursor colors and background in draw function for improved visual effect

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,6 @@
 let size = 42;
-let fill_color = 255;
-let stroke_color = 100;
+let fill_color = "#a5c3df";
+let stroke_color = "#10100f";
 let x = 0, y = 0; friction = 0.1;
 
 function setup() {
@@ -10,15 +10,18 @@ function setup() {
 }
 
 function draw() {
+    background(179, 30);
     updatecursor();
-   background (0, 35, 200, 80);
 }
 function updatecursor() {
-
-      fill(fill_color);
-    ellipse(mouseX, mouseY, size, size);
-   stroke (stroke_color);
-   x += (mouseX - x) * friction;
+    //
+    x += (mouseX - x) * friction;
    y += (mouseY - y) * friction;
+   //
+    let amt = map(x, 0, width, 0, 1);
+   fill_color = lerpColor(color("#a6c4e0"), color("#da8ca4"), amt);
+   fill(fill_color);
+    ellipse(mouseX, mouseY, size, size);
+   stroke(stroke_color);
     ellipse(x, y, size, size);
 }
